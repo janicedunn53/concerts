593df69d23c3e58259fde603ebc805d2681c1058
Janice Dunn <janicedunn53@gmail.com>
2015-08-28 09:53:14 -0700

    Add methods to tell what venues a band plays at and what bands play at a particular venue.

---
+++ spec/band_spec.rb
@@ -0,0 +1,12 @@
+require('spec_helper')
+
+describe(Band) do
+  describe('#venues') do
+    it("returns the venues a band plays at") do
+      venue1 = Venue.create({:name => "Greek Theatre", :city => "Los Angeles"})
+      venue2 = Venue.create({:name => "The Wiltern", :city => "Los Angeles"})
+      band = Band.new({:name => "Muse", :venue_ids => [venue1.id(), venue2.id()]})
+      expect(band.venues()).to(eq([venue1, venue2]))
+    end
+  end
+end
---
+++ spec/venue_spec.rb
@@ -0,0 +1,12 @@
+require('spec_helper')
+
+describe(Venue) do
+  describe('#bands') do
+    it("returns the bands that play at the venue") do
+      band1 = Band.create({:name => "Oingo Boingo"})
+      band2 = Band.create({:name => "Red Hot Chili Peppers"})
+      venue = Venue.new({:name => "Irvine Meadows Amphitheatre", :band_ids => [band1.id(), band2.id()]})
+      expect(venue.bands()).to(eq([band1, band2]))
+    end
+  end
+end
